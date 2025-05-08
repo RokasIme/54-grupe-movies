bf4f31e38b11e301b29daa6c383868da9b3d055f
Rokas Imelinskas <rokas.imelinskas@gmail.com>
2025-05-08 15:07:20 +0300

    movies by category

--- src/router/pageRouter.js
+++ src/router/pageRouter.js
@@ -2,6 +2,7 @@ import express from "express";
 import { PageHome } from "../pages/PageHome.js";
 import { Page404 } from "../pages/Page404.js";
 import { PageMovies } from "../pages/PageMovies.js";
+import { PageCategories } from "../pages/PageCategories.js";
 
 export const pageRouter = express.Router();
 
@@ -11,9 +12,9 @@ pageRouter.get("/movies/:movieTitle", (req, res) =>
   res.send(new PageMovies().render())
 );
 pageRouter.get("/movies-by-category", (req, res) =>
-  res.send(new PageMovies().render())
+  res.send(new PageCategories().render())
 );
 pageRouter.get("/movies-by-category/:categoryName", (req, res) =>
-  res.send(new PageMovies().render())
+  res.send(new PageMovies(req).render())
 );
 pageRouter.get("*error", (req, res) => res.send(new Page404().render()));
