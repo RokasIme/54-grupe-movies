7f093dc42fe68aeb64adf510e85236d6c184783e
Rokas Imelinskas <rokas.imelinskas@gmail.com>
2025-05-19 13:01:10 +0300

    port ENV

---
+++ src/env.js
@@ -0,0 +1,18 @@
+// išsiaiškinti norimo failo pavadinimą
+// perskaityti iš failo (jei jis egzistuoja)
+
+import dotenv from "dotenv";
+
+let envFile = ".env";
+
+for (const str of process.argv.slice(2)) {
+  if (str.startsWith("--env=")) {
+    envFile = str.split("=")[1];
+  }
+}
+
+dotenv.config({
+  path: envFile,
+});
+
+export const PORT = process.env.PORT ?? 3000;
